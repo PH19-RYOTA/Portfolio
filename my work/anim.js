5ba3a6474d836f3455c92d8ed38271eb0eba8665
YAMAMOTO-RYOTA <yamamoto17ryota@gmail.com>
2021-06-18 22:38:01 +0900

    update618

--- my work/anim.js
+++ my work/anim.js
@@ -17,14 +17,3 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
     $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
-
-/* iflame軽量化 */
-function init() {
-    var vidDefer = document.getElementsByTagName('iframe');
-    for (var i=0; i<vidDefer.length; i++) {
-        if(vidDefer[i].getAttribute('data-src')) {
-        vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-        } 
-    } 
-}
-window.onload = init;
